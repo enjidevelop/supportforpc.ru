edb0b0d73d7ef6a4f68046dd26a72ccd69b06906
Kustov Oleg <enjidevelop@gmail.com>
2016-02-26 23:36:41 +0300

    add start template v0.1.1

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,18 @@ var gulp         = require('gulp');
 var sass         = require('gulp-sass');
 var livereload   = require('gulp-livereload');
 var jade         = require('gulp-jade');
+var jadeInh      = require('gulp-jade-inheritance');
 var autoprefixer = require('gulp-autoprefixer');
 var gulpif       = require('gulp-if');
+var changed      = require('gulp-changed');
+var plumber      = require('gulp-plumber');
+var uglify       = require('gulp-uglify');
+var spritesmith  = require('gulp.spritesmith');
+var notify       = require('gulp-notify');
+var prettify     = require('gulp-prettify');
+var typograf     = require('gulp-typograf');
+
+var onError = notify.onError('Ошибка в <%= error.plugin %>');
 
 var paths = {};
 
@@ -33,7 +43,13 @@ paths.html = paths.buildBase + '/**/*.html';
 
 var buildCss = function() {
     return gulp.src(paths.dist.styles)
-        .pipe(sass())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .on('error', notify.onError({
+            message : 'Line: <%= error.lineNumber %>:' +
+            ' <%= error.message %>' +
+            '\n<%= error.fileName %>',
+            title   : '<%= error.plugin %>'
+        }))
         .on('error', function() {
             this.emit('end');
         })
@@ -64,7 +80,7 @@ gulp.task('sprites', function() {
         .pipe(gulpif(
             '*.png',
             gulp.dest(paths.buildBase + '/img'),
-            gulp.dest(paths.src.stylesBase + '/base')
+            gulp.dest(paths.dist.stylesBase + '/base')
         ))
         .pipe(livereload());
 });
@@ -81,7 +97,85 @@ gulp.task('sprites2x', function() {
         .pipe(gulpif(
             '*.png',
             gulp.dest(paths.buildBase + '/img'),
-            gulp.dest(paths.src.stylesBase + '/base')
+            gulp.dest(paths.dist.stylesBase + '/base')
         ))
         .pipe(livereload());
 });
+
+gulp.task('styles', ['sprites', 'sprites2x'], function() {
+    return buildCss();
+});
+
+gulp.task('css', function() {
+    return buildCss();
+});
+
+gulp.task('js-uglify', function jsTask() {
+    return gulp.src(paths.dist.scripts, {
+        base: paths.dist.scriptsBase
+    })
+        .pipe(changed(paths.build.scripts))
+        .pipe(plumber({
+            errorHandler: notify.onError({
+                message : 'Line: <%= error.lineNumber %>:' +
+                ' <%= error.message %>' +
+                '\n<%= error.fileName %>',
+                title   : '<%= error.plugin %>'
+            })
+        }))
+        .pipe(uglify({
+            outSourceMap: false
+        }))
+        .pipe(plumber.stop())
+        .pipe(gulp.dest(paths.build.scripts))
+        .pipe(livereload());
+});
+
+gulp.task('jade', function() {
+    return gulp.src(paths.dist.jade)
+        .pipe(changed(paths.build.jade, {extension: '.html'}))
+        .pipe(jadeInh({basedir: paths.dist.jadeBase}))
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(typograf({
+            lang: 'ru',
+            disable: ['ru/nbsp/centuries', 'common/number/fraction']
+        }))
+        .on('error', notify.onError({
+            message : 'Failed to compile html',
+            title   : 'Jade'
+        }))
+        .on('error', function() {
+            this.emit('end');
+        })
+        .pipe(prettify({
+            indent_size: 4
+        }))
+        .pipe(gulp.dest(paths.build.jade));
+});
+
+/**
+ * Watch task
+ */
+gulp.task('watch', ['build'], function watch() {
+    livereload.listen(function(err) {
+        if (err) {
+            return console.log('Livereload start failed');
+        }
+
+        gulp.watch(paths.dist.sprites, ['sprites']);
+        gulp.watch(paths.dist.sprites2x, ['sprites2x']);
+        gulp.watch(paths.dist.styles,  ['css']);
+        gulp.watch(paths.dist.scripts, ['js-uglify']);
+        gulp.watch(paths.dist.jade,    ['jade']);
+
+        gulp.watch(paths.html).on('change', function(file) {
+            livereload.changed(file.path);
+        });
+    });
+});
+
+// Run
+gulp.task('default', ['build']);
+
