b1fed44a190ec402d3ce6dd19d4f0bd11b182734
Kustov Oleg <enjidevelop@gmail.com>
2016-03-06 01:04:37 +0300

    fix feedback form

--- www/ajax/feedback.php
+++ www/ajax/feedback.php
@@ -1,30 +1,20 @@
-<?php
-
+<?
 $data = array(
     'ret' => 0
 );
 
-/* Осуществляем проверку вводимых данных и их защиту от враждебных
-скриптов */
-$name = htmlspecialchars($_POST["feedback_name"]);
-$phone = htmlspecialchars($_POST["feedback_phone"]);
-$subject = "Supportforpc.ru"
+$name = $_REQUEST["feedback_name"];
+$phone = $_REQUEST["feedback_phone"];
+$subject = "Supportforpc.ru";
+$myemail = "enjoy0707@mail.ru, oleg@it-nanny.ru";
 
-/* Устанавливаем e-mail адресата */
-$myemail = "enjoy0707@mail.ru";
+$message_to_myemail = "На сайте Supportforpc.ru заполнили форму!\n
+Имя отправителя: $name\n
+Tелефон: $phone";
 
-/* Проверяем заполнены ли обязательные поля ввода, используя check_input
-функцию */
-$name = check_input($_POST["feedback_name"], "Введите ваше имя!");
-$phone = check_input($_POST["feedback_phone"], "Укажите тему сообщения!");
-
-/* Создаем новую переменную, присвоив ей значение */
-$message_to_myemail = "На сайте Supportforpc.ru заполнили форму!
-Имя отправителя: $name
-Телефон: $phone";
-
-/* Отправляем сообщение, используя mail() функцию */
-mail($myemail, $subject, $message_to_myemail, $headers);
+if (!empty($name) && !empty($phone)) {
+    mail($myemail, $subject, $message_to_myemail);
+}
 
 $data['ret'] = 1;
 
